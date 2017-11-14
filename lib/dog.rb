8330eb70a311db075c11b2a73607daa7a597ac23
Michael Evans <michael@michaelgregoryevans.com>
2017-11-14 16:35:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,7 +34,7 @@ class Dog
       SQL
 
     DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute(SELECT last_insert_rowid() FROM dogs)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")
     self
   end
 

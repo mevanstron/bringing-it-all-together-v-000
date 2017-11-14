2d69ff78fa96253c8b08353109913e1c647ffd6d
Michael Evans <michael@michaelgregoryevans.com>
2017-11-14 16:28:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,4 +7,17 @@ class Dog
     @breed = breed
     @id = id
   end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
+
+    DB[:conn].execute(sql)
+  end
+  
 end
